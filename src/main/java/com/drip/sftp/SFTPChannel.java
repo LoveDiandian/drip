bb156026d7288b6c377ad67d7df4bb8ee4102442
zhouguangsheng@he-tailife.com <zhouguangsheng@he-tailife.com>
2017-03-24 16:53:22 +0800

    测试多目标

--- src/main/java/com/drip/sftp/SFTPChannel.java
+++ src/main/java/com/drip/sftp/SFTPChannel.java
@@ -18,6 +18,7 @@ public class SFTPChannel {
 
     public ChannelSftp getChannel(Map<String, String> sftpDetails, int timeout) throws JSchException {
 
+
         String ftpHost = sftpDetails.get(SFTPConstants.SFTP_REQ_HOST);
         String port = sftpDetails.get(SFTPConstants.SFTP_REQ_PORT);
         String ftpUserName = sftpDetails.get(SFTPConstants.SFTP_REQ_USERNAME);
