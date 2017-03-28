c304e2b448e8e934518f6e33feff2a4de009e1eb
zhouguangsheng@he-tailife.com <zhouguangsheng@he-tailife.com>
2017-03-28 17:46:07 +0800

    HttpclientUtil-4

--- src/main/java/com/drip/http/HttpClientUtil.java
+++ src/main/java/com/drip/http/HttpClientUtil.java
@@ -20,9 +20,10 @@ public class HttpClientUtil {
     private IdleConnectionMonitorThread idleConnectionMonitorThread = null;
     private Gson gson = new Gson();
 
-
-
     //第三次提交
+
+    //第四次提交
+
 
     class IdleConnectionMonitorThread extends Thread {
         private final PoolingHttpClientConnectionManager connMgr;
