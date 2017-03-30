364c6fca72f92380f3cd9f372340da362b746c32
zhouguangsheng@he-tailife.com <zhouguangsheng@he-tailife.com>
2017-03-30 13:58:26 +0800

    Perfect http util

--- src/main/java/com/drip/http/HttpClientUtil.java
+++ src/main/java/com/drip/http/HttpClientUtil.java
@@ -1,8 +1,19 @@
 package com.drip.http;
 
 import com.google.gson.Gson;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.config.SocketConfig;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
@@ -20,14 +31,70 @@ public class HttpClientUtil {
     private IdleConnectionMonitorThread idleConnectionMonitorThread = null;
     private Gson gson = new Gson();
 
-    //第一次
-    //第二次
-    //第三次
+    private Logger log = LoggerFactory.getLogger(HttpClientUtil.class);
 
+    private HttpClientUtil() {
+        init();
+    }
+
+    private static class SingletonContainer {
+        private static HttpClientUtil _hinstance = new HttpClientUtil();
+    }
+
+    /*单例写法*/
+    public HttpClientUtil getInstance() {
+        return SingletonContainer._hinstance;
+    }
+
+    private void init() {
+        try {
+            ConnectionSocketFactory plainsf = PlainConnectionSocketFactory.getSocketFactory();
+            SSLContextBuilder builder = new SSLContextBuilder();
+            builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build(), NoopHostnameVerifier.INSTANCE);
+            Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create().register("http", plainsf)
+                    .register("https", sslsf).build();
+            poolConnManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+            // Increase max total connection to 200
+            poolConnManager.setMaxTotal(connect_max_total);
+            // Increase default max connection per route to 20
+            poolConnManager.setDefaultMaxPerRoute(connect_max_per_route);
+            SocketConfig socketConfig = SocketConfig.custom().setSoTimeout(socket_timeout).build();
+            poolConnManager.setDefaultSocketConfig(socketConfig);
+
+            // 定期清理无效、空闲连接
+            idleConnectionMonitorThread = new IdleConnectionMonitorThread(poolConnManager);
+            idleConnectionMonitorThread.start();
+            // 增加关闭钩子
+            addHook();
+        } catch (Exception e) {
+            log.warn("NetUtil init Exception", e);
+        }
+    }
+
+    private void addHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                log.info("shutdown hook get");
+                close();
+                log.info("shutdown hook end");
+            }
+        }));
+    }
+
+    private void close() {
+        if (poolConnManager != null) {
+            poolConnManager.close();
+        }
+        if (idleConnectionMonitorThread != null) {
+            idleConnectionMonitorThread.shutdown();
+        }
+    }
 
     class IdleConnectionMonitorThread extends Thread {
         private final PoolingHttpClientConnectionManager connMgr;
-        private volatile boolean                         shutdown;
+        private volatile boolean shutdown;
 
         public IdleConnectionMonitorThread(PoolingHttpClientConnectionManager connMgr) {
             super();
